babd196fb2953b28f44bad7e16a56f7fb43702a8
halhassani <61643610+halhassani@users.noreply.github.com>
2023-07-04 15:55:18 -0700

    Baseline Code for TDC7200 Driver Files
    Yus.

---
+++ Core/Inc/TDC7200_driver.h
@@ -0,0 +1,15 @@
+/*
+ * TDC7200_driver.h
+ *
+ *  Created on: Jul 4, 2023
+ *      Author: Halha
+ */
+
+#ifndef INC_TDC7200_DRIVER_H_
+#define INC_TDC7200_DRIVER_H_
+
+
+
+
+
+#endif /* INC_TDC7200_DRIVER_H_ */
---
+++ Core/Src/TDC7200_driver.c
@@ -0,0 +1,8 @@
+/*
+ * TDC7200_driver.c
+ *
+ *  Created on: Jul 4, 2023
+ *      Author: Halha
+ */
+
+
