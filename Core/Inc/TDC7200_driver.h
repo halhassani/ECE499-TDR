a654b76ee75e3d181c91ac443ac012d017652603
halhassani <61643610+halhassani@users.noreply.github.com>
2023-07-06 23:25:23 -0700

    TDC: Bitfield defines for a few registers
    Yus.

--- Core/Inc/TDC7200_driver.h
+++ Core/Inc/TDC7200_driver.h
@@ -43,28 +43,78 @@
 #define TDC_CALIBRATION1					0X1B
 #define TDC_CALIBRATION2					0X1C
 
+
+//Bitfield defines for individual bits in various registers
+#define BIT_SET										(1<<0)
+#define BIT_RESET									(0<<0)
+
+/***************** ConfigReg_1 ******************/
+#define MEASURE_MODE_1						BIT_RESET
+#define START_MEASUREMENT					BIT_SET
+
+#define START_EDGE_RISING					BIT_RESET
+#define START_EDGE_FALLING				BIT_SET
+
+#define STOP_EDGE_RISING					BIT_RESET
+#define STOP_EDGE_FALLING					BIT_SET
+
+#define TRIGG_EDGE_RISING					BIT_RESET
+#define TRIGG_EDGE_FALLING				BIT_SET
+/************************************************/
+/************************************************/
+
+
+/***************** ConfigReg_2 ******************/
+#define NUM_STOP_SINGLE						0
+#define NUM_STOP_TWO							1
+#define NUM_STOP_THREE						2
+#define NUM_STOP_FOUR							3
+#define NUM_STOP_FIVE							4
+
+#define AVG_CYCLES_1							0
+#define AVG_CYCLES_2							1
+#define AVG_CYCLES_4							2
+#define AVG_CYCLES_8							3
+#define AVG_CYCLES_16							4
+#define AVG_CYCLES_32							5
+#define AVG_CYCLES_64							6
+#define AVG_CYCLES_128						7
+
+#define CALIBRATION2_PERIOD_2				0
+#define CALIBRATION2_PERIOD_10			1
+#define CALIBRATION2_PERIOD_20			2
+#define CALIBRATION2_PERIOD_40			3
+/************************************************/
+/************************************************/
+
+
+
 //Register Structures to make low-level TDC configurations easier/faster ~~ABSTRACTION~~
 typedef struct
 {
 	uint8_t start_measurement : 1;	//0 = no effect, 1 = Start new measurement
-	uint8_t measurement_mode 	: 2;	//00 = Measurement Mode 1 (which we want), 01 = Mode 2,  10/11 = Reserved
+	uint8_t measurement_mode 	: 2;	//00 = Measurement_Mode_1 (which we want), 01 = Mode 2,  10/11 = Reserved
 	uint8_t start_edge				: 1;	//0 = measure starts on RISING edge of START signal, 1 = FALLing edge
-	uint8_t stop_edge					: 1;	//...
-	uint8_t trigger_edge			: 1;
-	uint8_t parity_enable			: 1;
-	uint8_t force_calibration :	1;	//bit index 7
+	uint8_t stop_edge					: 1;	//0 = Rising edge, 		1 = Falling edge
+	uint8_t trigger_edge			: 1;	//0 = Rising edge, 		1 = Falling edge
+
+			//0 = Parity bit disabled for MeasurementResultRegisters (TIME1-6, CLK_CNT1-5...etc) ie: Parity bit always 0
+	uint8_t parity_enable			: 1;		//1 = Parity bit enabled (Even parity)
+
+	//0 = Calibration not performed after int. measurement,  1 = calibration always performed at end
+	uint8_t force_calibration :	1;
 } TDC7200_ConfigReg1_t;
 
 typedef struct
 {
-	uint8_t number_of_stops 			: 3;
+	uint8_t number_of_stops 			: 3;	//datasheet pg.26
 	uint8_t average_cycles				: 3;
 	uint8_t calibration2_periods	: 2;
 }TDC7200_ConfigReg2_t;
 
 typedef struct
 {
-	uint8_t new_measure_int					: 1;
+	uint8_t new_measure_int					: 1;	//datasheet pg.27
 	uint8_t course_counter_ovf_int	: 1;
 	uint8_t clock_counter_ovf_int		: 1;
 	uint8_t measure_started_flag		: 1;
@@ -75,7 +125,7 @@ typedef struct
 
 typedef struct
 {
-	uint8_t new_measure_mask				: 1;
+	uint8_t new_measure_mask				: 1;	//datasheet pg.28
 	uint8_t coarse_counter_ovf_mask	: 1;
 	uint8_t clock_counter_ovf_mask	: 1;
 	uint8_t reserved								: 5;
@@ -83,36 +133,36 @@ typedef struct
 
 typedef struct
 {
-	uint8_t coarse_counter_ovf_H 		: 8;
+	uint8_t coarse_counter_ovf_H 		: 8;	//Coarse Counter Overflow Value, upper 8 Bit ("High" byte)
 } TDC7200_CoarseCounterOVF_H_Reg_t;
 
 typedef struct
 {
-	uint8_t coarse_counter_ovf_L 		: 8;
+	uint8_t coarse_counter_ovf_L 		: 8;	//Coarse Counter Overflow Value, lower 8 Bit ("Low" byte)
 } TDC7200_CoarseCounterOVF_L_Reg_t;
 
 typedef struct
 {
-	uint8_t clock_counter_ovf_H 		: 8;
+	uint8_t clock_counter_ovf_H 		: 8;	//CLOCK Counter Overflow_Value, UPPER 8 Bit ("High" byte)
 } TDC7200_ClockCounterOVF_H_Reg_t;
 
 typedef struct
 {
-	uint8_t clock_counter_ovf_L 		: 8;
+	uint8_t clock_counter_ovf_L 		: 8;	//CLOCK Counter Overflow_Value, LOWER 8 Bit ("Low" byte)
 } TDC7200_ClockCounterOVF_L_Reg_t;
 
 typedef struct
 {
-	uint8_t clock_counter_stop_mask_H	: 8;
+	uint8_t clock_counter_stop_mask_H	: 8;	//CLOCK Counter STOP_Mask, UPPER 8 Bit ("High" byte)
 } TDC7200_ClockCounterStopMask_H_Reg_t;
 
 typedef struct
 {
-	uint8_t clock_counter_stop_mask_L	: 8;
+	uint8_t clock_counter_stop_mask_L	: 8;	//CLOCK Counter STOP_Mask, LOWER 8 Bit ("Low" byte)
 } TDC7200_ClockCounterStopMask_L_Reg_t;
 
 // ^^^^^^ These 10 registers are read/write capable. ^^^^^^
-//The remaining 10 registers in the datasheet are read-only, so no abstraction code written for those
+//The remaining 10 registers in the datasheet are read-only, so no abstraction code written for those.
 
 
 
